82d223701b1e5464b5b660aa3c00fea606fbca46
ambiwalencja <helena.a.sokolowska@gmail.com>
2021-04-14 00:26:56 +0200

    cosmetic changes to user messages

--- parcelshop.py
+++ parcelshop.py
@@ -9,14 +9,14 @@ item_weight = 1 # initial value is 1 so we can enter the loop
 condition1 = item_weight >= 1
 condition2 = item_weight <= 10
 # welcome message
-print("""Welcome to parcel loading service. 
+print("""Welcome to our parcelshop! We will send all your items! 
 Please type the weight of each item you want to send respectively.
 Remember: the weight of one item must be between 1 and 10 kg!")
 When you are finished entering your items, type "0" as you have 0 more kilograms to send :)""")
 
 #entering data
 while item_weight != 0: # until the user types 0 we repeat adding items
-    item_weight = int(input("Type the weight of the item:"))
+    item_weight = int(input("Type the weight of the item: "))
     if item_weight >= 1 and item_weight <= 10:  # if the item has the right weight (when i used conditions, it did not work)
         if parcel_weight + item_weight <= 20:  # if the item fits
             parcel_weight += item_weight  # we add the item to the parcel
@@ -25,7 +25,7 @@ while item_weight != 0: # until the user types 0 we repeat adding items
             total_weight += parcel_weight  # we add the weight of current parcel
             parcel_weight = item_weight # last item goes to new parcel
     else:
-        print("ERROR - ")  # if the item is not the right weight
+        print("ERROR - YOU ARE DOING IT WRONG")  # if the item is not the right weight
         break
 total_weight += parcel_weight  # we add the weight of the last parcel
 if total_weight > 0:
